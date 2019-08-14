4b089dba27f0220d44c958c644cdf9c8443bc183
gougongwei <guogongwei@howzhi.com>
2019-08-14 22:49:09 +0800

    fix

--- config.py
+++ config.py
@@ -4,4 +4,7 @@ _basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 
+SQLALCHEMY_DATABASE_URI = 'mysql://XXX:XXX@localhost:3306/XXX
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
 del os
