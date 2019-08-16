03ecc1ac57b2e877d06f8583d8e2680b4641445a
gougongwei <guogongwei@howzhi.com>
2019-08-16 13:39:55 +0800

    fix

--- scrum_chart/__init__.py
+++ scrum_chart/__init__.py
@@ -1,15 +1,39 @@
 import datetime
 from flask import Flask, request, session, g, render_template
+from operator import itemgetter, attrgetter
 
 # 加载 app 并添加基础配置
 app = Flask(__name__)
 app.config.from_object('config')
 
-from scrum_chart.database import Scrum, db
+from scrum_chart.database import db, Scrum, ScrumSprint
 
 @app.route('/')
 def index():
-    scrums = Scrum.query.all()
+    scrum_No = request.args.get('scrum_No')
+    if scrum_No != None:
+        source = getdataFromDb(scrum_No)
+    else:
+        source = getdataFromQuery(request)
+
+    return render_template('index.html',
+        source = source,
+        scrums = Scrum.query.all()
+    )
+
+@app.route('/chart')
+def chart():
+    scrum_No = request.args.get('scrum_No')
+    source = {'dates': [], 'stand_values':[], 'actual_values': []}
+    if scrum_No != None:
+        source = getdataFromDb(scrum_No)
+
+    return render_template('chart.html',
+        source = source,
+        scrums = Scrum.query.all()
+    )
+
+def getdataFromQuery(request):
     total = request.args.get('total', 0)
     start = request.args.get('start', '2019-07-01')
     start = datetime.datetime.strptime(start, '%Y-%m-%d')
@@ -17,38 +41,32 @@ def index():
     end = datetime.datetime.strptime(end, '%Y-%m-%d')
     days = calc_days(start, end)
     stand_line = get_stand_line_value(start, end, total, days)
-
     actual_values = request.args.get('actual', [])
-
     source = {
         'dates': stand_line[1],
         'stand_values':stand_line[0],
         'actual_values': actual_values
     }
-    return render_template('index.html',
-        source = source,
-        scrums = scrums
-    )
 
-@app.route('/chart')
-def chart():
-    scrum_No = request.args.get('scrum_No')
+    return source
+
+def getdataFromDb(scrum_No):
     scrum = Scrum.query.filter_by(No = scrum_No).first()
-    print(scrum.start_date)
     start_date = datetime.datetime.strptime(scrum.start_date, '%Y-%m-%d')
     end_date = datetime.datetime.strptime(scrum.end_date, '%Y-%m-%d')
     days = calc_days(start_date, end_date)
     stand_line = get_stand_line_value(start_date, end_date, scrum.total_sprint, days)
+    scrum_sprints = ScrumSprint.query.filter_by(scrum_id = scrum.id).all()
+    scrum_sprints = sorted(scrum_sprints, key = attrgetter('day'))
+    actual_values = []
+    for item in scrum_sprints:
+        actual_values.append(item.left_sprint)
 
-    scrums = Scrum.query.all()
-    return render_template('chart.html',
-        source = {
+    return {
             'dates': stand_line[1],
             'stand_values':stand_line[0],
-            'actual_values': []
-        },
-        scrums = scrums
-    )
+            'actual_values': actual_values
+        }
 
 def calc_days(start_date, end_date):
     days = 1
--- scrum_chart/database.py
+++ scrum_chart/database.py
@@ -6,8 +6,18 @@ db = SQLAlchemy(app)
 class Scrum(db.Model):
     __tablename__ = 'scrum'
     id = db.Column(db.Integer, primary_key=True)
-    project_id = db.Column(db.String(255))
+    project_id = db.Column(db.Integer)
     No = db.Column(db.String(255))
     total_sprint = db.Column(db.Integer)
     start_date = db.Column(db.String(255))
     end_date = db.Column(db.String(255))
+
+
+
+class ScrumSprint(db.Model):
+    __tablename__ = 'scrum_sprint'
+    id = db.Column(db.Integer, primary_key=True)
+    scrum_id = db.Column(db.Integer)
+    day = db.Column(db.String(255))
+    left_sprint = db.Column(db.Integer)
+
